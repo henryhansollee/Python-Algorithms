e47e508b00c75e951cd0e255ff4006817574854e
henryhansollee <henryhansollee@gmail.com>
2020-02-13 06:10:29 +0900

    Solve SWEA 2027. 대각선 출력하기

--- Problem.py
+++ Problem.py
@@ -1,7 +1,8 @@
 import sys
 sys.stdin = open("Input.txt", "r")
 
-t = int(input())
-for tc in range(1, t+1):
-    a, b = map(int, input().split())
-    print("#{} {} {}".format(tc, a // b, a % b))
+print('#++++')
+print('+#+++')
+print('++#++')
+print('+++#+')
+print('++++#')
