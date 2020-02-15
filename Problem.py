8cea02565a29bcdf9609d6e2ae943ed212e35f23
henryhansollee <henryhansollee@gmail.com>
2020-02-16 06:24:52 +0900

    Solve SWEA 1933. 간단한 N 의 약수

--- Problem.py
+++ Problem.py
@@ -1,8 +1,7 @@
 import sys
 sys.stdin = open("Input.txt", "r")
 
-a, b = map(int, input().split())
-print(a+b)
-print(a-b)
-print(a*b)
-print(a//b)
+N = int(input())
+for i in range(1, N+1):
+    if N % i == 0:
+        print(i, end=' ')
