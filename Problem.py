3abbf58bff664f59f166e6fa8eb201b6c9deb301
henryhansollee <henryhansollee@gmail.com>
2020-02-18 08:32:18 +0900

    Solve SWEA 2019. 더블더블

--- Problem.py
+++ Problem.py
@@ -1,18 +1,6 @@
 import sys
 sys.stdin = open("Input.txt", "r")
 
-A, B = map(int, input().split())
-ans = ''
-if A == 1 and B == 2:
-    ans = 'B'
-elif A == 1 and B == 3:
-    ans = 'A'
-elif A == 2 and B == 1:
-    ans = 'A'
-elif A == 2 and B == 3:
-    ans = 'B'
-elif A == 3 and B == 1:
-    ans = 'B'
-elif A == 3 and B == 2:
-    ans = 'A'
-print(ans)
+n = int(input())
+for i in range(n+1):
+    print(2 ** i, end=' ')
